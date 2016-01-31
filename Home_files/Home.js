f390d5a59bb7b4b0df82142f7ada9c0d8330cde0
metaarch <xwang.metaarch@outlook.com>
2016-01-31 17:14:38 +0100

    website V1.0

---
+++ Home_files/Home.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20160131
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Home_files/notebook_ul.png'),IWCreateImage('Home_files/notebook_top.png'),IWCreateImage('Home_files/notebook_ur.png'),IWCreateImage('Home_files/notebook_right.png'),IWCreateImage('Home_files/notebook_lr.png'),IWCreateImage('Home_files/notebook_bottom.png'),IWCreateImage('Home_files/notebook_ll.png'),IWCreateImage('Home_files/notebook_left.png')],null,0,1.000000,3.000000,2.000000,1.000000,3.000000,18.000000,16.000000,17.000000,19.000000,76.000000,123.000000,79.000000,122.000000,null,null,null,0.400000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
