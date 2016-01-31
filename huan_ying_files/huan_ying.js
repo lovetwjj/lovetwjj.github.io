b686eba9c52ab42f6bb8f3b99008f766ba6eee66
metaarch <xwang.metaarch@outlook.com>
2016-01-31 16:23:49 +0100

    Initial commit

--- huan_ying_files/huan_ying.js
+++ huan_ying_files/huan_ying.js
@@ -1,11 +1,11 @@
 // Created by iWeb 3.0.4 local-build-20160131
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:5,offset:new IWPoint(-1.4863,1.3383),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,9,10,255),url:'huan_ying_files/stroke.png'},{rect:new IWRect(-6,-1,15,10),url:'huan_ying_files/stroke_1.png'},{rect:new IWRect(9,-1,192,10),url:'huan_ying_files/stroke_2.png'},{rect:new IWRect(201,3,10,6),url:'huan_ying_files/stroke_3.png'},{rect:new IWRect(201,9,10,255),url:'huan_ying_files/stroke_4.png'},{rect:new IWRect(-7,-7,0,0),url:null},{rect:new IWRect(-7,-7,0,0),url:null},{rect:new IWRect(1,264,8,1),url:'huan_ying_files/stroke_5.png'}],new IWSize(206,262)),stroke_1:new IWStrokeParts([{rect:new IWRect(-1,10,10,252),url:'huan_ying_files/stroke_6.png'},{rect:new IWRect(-6,-1,15,11),url:'huan_ying_files/stroke_7.png'},{rect:new IWRect(9,-1,395,11),url:'huan_ying_files/stroke_8.png'},{rect:new IWRect(404,4,1,6),url:'huan_ying_files/stroke_9.png'},{rect:new IWRect(-7,-7,0,0),url:null},{rect:new IWRect(-7,-7,0,0),url:null},{rect:new IWRect(-7,-7,0,0),url:null},{rect:new IWRect(1,262,8,1),url:'huan_ying_files/stroke_10.png'}],new IWSize(400,260))});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('huan_ying_files/huan_yingMoz.css')
-Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixupAllIEPNGBGs();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
